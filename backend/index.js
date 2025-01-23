1bfcb6c91fd4398effa3952cf86ad3f2e3c8d36a
mayur-driod <settymayurk@gmail.com>
2025-01-23 12:24:36 +0530

    milestone 3

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { connectDB } = require('./src/Database/db');
+const app = express();
+
+require('dotenv').config({
+    path: './src/config/.env'
+});
+const port = process.env.PORT || 8080;
+const url = process.env.db_url;
+
+app.listen(port, async() => {
+
+  try{
+    await connectDB(url);
+    console.log(`Server is running on port ${port}`);
+  }
+  catch(error){
+    console.error(error);
+  }
+});
+
+app.get('/', (req, res) => {    
+    res.send('Hello World!');
+});
