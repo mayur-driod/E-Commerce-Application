22032e58679fe6d57f73b2f4642703442a1c8dec
mayur-driod <settymayurk@gmail.com>
2025-01-25 13:42:21 +0530

    made the signup form

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Login } from './Component/Login'
+import { Signup } from './Component/Signup'
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   )
