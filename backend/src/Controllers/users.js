6c276da342de2f3781cbb69a4f630c9da49580a7
mayur-driod <settymayurk@gmail.com>
2025-01-29 11:05:28 +0530

    2nd

--- backend/src/Controllers/users.js
+++ backend/src/Controllers/users.js
@@ -1,61 +1,70 @@
-const { Router } = require("express");
+const {Router}= require("express");
 const userModel = require("../Model/userModel");
-const userRouter = Router();
+const { upload } = require("../../multer");
+const jwt = require('jsonwebtoken');
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-require('dotenv').config({ path: './src/config/.env' });
+const userrouter = Router();
+require('dotenv').config({  path:'./src/config/.env'});
 
 const secret = process.env.secretkey;
 
-userRouter.post("/create-user",upload.single("file"), async (req, res, next) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(400).send("All fields are required");
-  }
+userrouter.post("/create-user",async(req,res)=>{
+    const {name, email, password} = req.body;
+    const userEmail = await userModel.findOne({email});
+    if (userEmail) {
+        return next(new ErrorHandler("User already exists", 400));
+      }
 
-  const userEmail = await userModel.findOne({ email });
-  if (userEmail) {
-    return res.status(400).send("User already exists");
-  }
+      const filename = req.file.filename ;
+      const fileUrl = path.join(filename);
+   
+    bcrypt.hash(password, 10,async function(err, hash) {
 
-  bcrypt.hash(password, 10, async function (err, hash) {
-    if (err) {
-      return next(err);
+        await userModel.create({
+            name:name,
+            email:email,
+            password:hash,
+            // avatar:fileUrl
+
+        })
+
+        })
+
+
+});
+
+
+userrouter.post("/login",async(req,res)=>{
+    const {email, password} = req.body;
+    const check= await userModel.findOne({email:email});
+        console.log(check);
+    if(!check){
+        return res.status(400).json({message:"User not found"});
     }
 
-    await userModel.create({
-      name: name,
-      email: email,
-      password: hash,
-      // avatar: fileUrl
+    bcrypt.compare(password,check.password , function(err, result) {
+       
+        if(err){
+            return res.status(400).json({message:"Invalid bcrpyt compare"});
+        }
+        if(result){
+            
+            jwt.sign({email:email},secret,(err,token)=>{
+                if(err){
+                    return res.status(400).json({message:"Invalid jwt"});
+                }
+
+                console.log(token);
+                res.status(200).json({token:token});    
+            })
+
+
+        }   
+        else{
+            return res.status(400).json({message:"Invalid password"});
+        }
     });
 
-    res.status(201).send("User created successfully");
-  });
-});
+})
 
-userRouter.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const check = await userModel.findOne({ email: email });
-
-  if (!check) {
-    return res.status(400).send("User not found");
-  }
-
-  const pass = await bcrypt.compare(password, check.password);
-
-  if (!pass) {
-    return res.status(400).send("Password is incorrect");
-  }
-
-  jwt.sign({ email: email },secret,(err,token)=>{
-    if(err){
-      return res.status(400).send("Token creation failed");
-    }
-    console.log(token);
-    res.status(200).send(token);
-  })
-  res.send("Login successful");
-});
-
-module.exports = userRouter;
+module.exports = userrouter;
