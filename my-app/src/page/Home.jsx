0e9d06696e9f99d4e1af7bd3962277961578a92b
mayur-driod <settymayurk@gmail.com>
2025-01-29 12:30:34 +0530

    changed photo

--- my-app/src/page/Home.jsx
+++ my-app/src/page/Home.jsx
@@ -4,13 +4,13 @@ import { Productcard } from '../Component/Productcard'
 const productdetails = [
     {
     name: "Product1",
-    image: "https://inspgr.id/app/uploads/2015/03/product-design-koziol-04.jpg",
+    image: "https://picsum.photos/200/300",
     price: "1000",
     description: "Description1"
     },
     {
     name: "Product2",
-    image: "https://i.pinimg.com/originals/de/28/b9/de28b979a75113cdb0c626f200d409bf.jpg",
+    image: "https://picsum.photos/200/300",
     price: "2000",
     description: "Description2"
     }
