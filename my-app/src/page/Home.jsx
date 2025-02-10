7690a17bd688aa303585ce96d341cde75969b64d
mayur-driod <settymayurk@gmail.com>
2025-02-10 13:23:45 +0530

    first draft

--- my-app/src/page/Home.jsx
+++ my-app/src/page/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { Productcard } from '../Component/Productcard'
 
 // const productdetails = [
@@ -22,7 +22,24 @@ export const Home = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  
+  useEffect(() => {
+    fetch("http://localhost:3000/product/get-products")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+      setProductdetails(data.products);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("❌ Error fetching products:", err);
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
 
     return (
       <div>
