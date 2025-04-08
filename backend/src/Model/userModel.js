7e8c260a3288542d3a21b813a0562db780b28414
mayur-driod <settymayurk@gmail.com>
2025-04-08 09:33:25 +0530

    backend working!

--- backend/src/Model/userModel.js
+++ backend/src/Model/userModel.js
@@ -66,28 +66,28 @@ const userSchema = new Schema({
           },
         }
       ],
-    //   role:{
-    //     type: String,
-    //     default: "user",
-    //   },
-    //   avatar:{
-    //     public_id: {
-    //       type: String,
-    //       required: true,
-    //     },
-    //     url: {
-    //       type: String,
-    //       required: true,
-    //     },
-    //  },
-    //  createdAt:{
-    //   type: Date,
-    //   default: Date.now(),
-    //  },
-    //  resetPasswordToken: String,
-    //  resetPasswordTime: Date,
+      role:{
+        type: String,
+        default: "user",
+      },
+      avatar:{
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+     },
+     createdAt:{
+      type: Date,
+      default: Date.now(),
+     },
+     resetPasswordToken: String,
+     resetPasswordTime: Date,
 });
 
 const userModel = model('User', userSchema);
 
-model.exports = userModel;
+module.exports = userModel;
